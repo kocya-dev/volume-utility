77a492d83b42918fec2963c97a04e95a6bdb4927
kocya <sample@test.co.jp>
2025-01-01 03:11:22 +0900

    アプリごとの音量設定クラスと管理クラスの追加

---
+++ volume-utility/Controller/VolumeSetting.cs
@@ -0,0 +1,31 @@
+﻿namespace volume_utility.Controller
+{
+    public class VolumeSetting
+    {
+        /// <summary>
+        /// プロセス名
+        /// </summary>
+        public string ProcessName { get; private set; }
+        /// <summary>
+        /// アプリケーション名
+        /// </summary>
+        public string ApplicationName { get; private set; }
+        /// <summary>
+        /// ボリューム
+        /// </summary>
+        public int Volume { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <param name="applicationName"></param>
+        /// <param name="volume"></param>
+        public VolumeSetting(string processName, string applicationName, int volume)
+        {
+            ProcessName = processName;
+            ApplicationName = applicationName;
+            Volume = volume;
+        }
+    }
+}
