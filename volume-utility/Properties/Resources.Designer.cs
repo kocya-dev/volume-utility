e35d292f57331739870ac83097a1838473fc1e57
kocya <sample@test.co.jp>
2025-01-01 03:10:05 +0900

    ゴミ箱アイコン追加

--- volume-utility/Properties/Resources.Designer.cs
+++ volume-utility/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace volume_utility.Properties {
         }
         
         /// <summary>
+        ///   型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static System.Drawing.Bitmap garbage {
+            get {
+                object obj = ResourceManager.GetObject("garbage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   (アイコン) に類似した型 System.Drawing.Icon のローカライズされたリソースを検索します。
         /// </summary>
         internal static System.Drawing.Icon speaker_black {
